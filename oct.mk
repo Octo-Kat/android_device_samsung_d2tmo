1ed56e024560ef26e5ef42c2f042d98f0e7ec94a
treken <chupekevin@gmail.com>
2014-01-30 22:19:00 -0500

    Fix derps
    Copy & paste is either friend or foe, in this case foe!

--- oct.mk
+++ oct.mk
@@ -8,10 +8,10 @@ $(call inherit-product, vendor/oct/config/gsm.mk)
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=d2uc \
+    PRODUCT_NAME=d2tmo \
     TARGET_DEVICE=d2tmo \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/T999UVDMD5:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K T999UVDMD5 release-keys"
+    BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.1.2/JZO54K/T999UVDMD5:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2tmo-user 4.1.2 JZO54K T999UVDMD5 release-keys"
 
 PRODUCT_NAME := oct_d2tmo
 PRODUCT_DEVICE := d2tmo
